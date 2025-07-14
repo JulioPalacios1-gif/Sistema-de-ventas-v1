1dd10fcd408dcb7e2ff0d84924049436b3141492
Fernando Mena <20250091@ricaldone.edu.sv>
2025-07-13 20:07:38 -0600

    Actualización de base de datos 2

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -7,116 +7,123 @@ USE SISTEMA_DE_VENTA
 GO
 
 create table ROL(
-IdRol int primary key identity,
-nombreRol varchar (50),
-Descripcion varchar (50),
-FechaCreacion datetime default getdate()
-)
-create table USUARIO(
-IdUsuario int primary key identity,
-Documento varchar (100),
-NombreCompleto varchar (50),
-Correo varchar (50),
-Clave varchar (50),
-rol_id int,
-foreign key (rol_id) references ROL(IdRol),
-Estado bit,
-FechaCreacion datetime default getdate()
-)
+IdRol int primary key identity(1,1),
+nombreRol varchar (50) not null,
+descripcionRol varchar (50) not null,
+FechaCreacionRol datetime default getdate());
+GO
 
+create table USUARIO(
+IdUsuario int primary key identity(1,1),
+DocumentoUsuario varchar (100) unique not null,
+nombreCompletoUsuario varchar (60) not null,
+correoUsuario varchar (50) unique not null,
+Clave varchar (50) not null,
+rol_id int not null,
+foreign key (rol_id) references ROL(IdRol),
+Estado bit not null,
+FechaCreacionUsuario datetime default getdate());
 go
 
 create table CATEGORIA(
-IdCategoria int primary key identity,
-Descripcion varchar (100),
-Estado bit,
-FechaCreacion datetime default getdate()
-)
-
+IdCategoria int primary key identity(1,1),
+descripcionCategoria varchar (100) not null,
+Estado bit not null,
+FechaCreacionCategoria datetime default getdate())
 go
 
 create table PROVEEDOR(
-IdProveedor int primary key identity,
-Documento varchar (100),
-RazonSocial varchar (50),
-Correo varchar (50),
-Telefono varchar (50),
-Estado bit,
-FechaCreacion datetime default getdate()
-)
-
+IdProveedor int primary key identity(1,1),
+documentoProveedor varchar (100) unique not null,
+razonSocialProveedor varchar (50) not null,
+correoProveedor varchar (50) unique not null,
+telefonoProveedor varchar (50) not null,
+Estado bit not null,
+FechaCreacionProveedor datetime default getdate());
 go
 
 create table PRODUCTO(
-IdProducto int primary key identity,
-Codigo varchar (50),
-Nombre varchar (75),
-Descripcion varchar (100),
-categoria_id int,
+IdProducto int primary key identity(1,1),
+codigoProducto varchar (50) unique not null,
+nombreProducto varchar (75) not null,
+descripcionProducto varchar (100) not null,
+categoria_id int not null,
 foreign key (categoria_id) references CATEGORIA(IdCategoria),
-proveedor_id int,
+proveedor_id int not null,
 foreign key (proveedor_id) references PROVEEDOR(IdProveedor),
 Stock int not null default 0,
 PrecioCompra decimal (10, 2) default 0,
 PrecioVenta decimal (10, 2) default 0,
-Estado bit,
-FechaRegistro datetime default getdate()
-)
+Estado bit not null,
+FechaRegistroProducto datetime default getdate());
 go
 
 create table CLIENTE(
-IdCliente int primary key identity,
-Documento varchar (100),
-NombreCompleto varchar (100),
-Correo varchar (50),
-Telefono varchar (50),
-Estado bit,
-FechaCreacion datetime default getdate()
-)
+IdCliente int primary key identity(1,1),
+documentoCliente varchar (100) unique not null,
+nombreCompletoCliente varchar (100) not null,
+correoCliente varchar (50) unique not null,
+telefonoCliente varchar (50) not null,
+Estado bit not null,
+FechaCreacionCliente datetime default getdate());
 go
+
+create table TipoDocumentoCompra(
+idTipoDocumentoCompra int identity(1,1) primary key,
+nombreDocumentoCompra varchar (20));
+go
+
 create table COMPRA(
-IdCompra int primary key identity,
-usuario_id int,
+IdCompra int identity(1,1) primary key,
+usuario_id int not null,
 foreign key (usuario_id) references USUARIO(IdUsuario),
-TipoDocumento varchar (50),
-NumeroDocumento varchar (50),
+tipoDocumentoCompra_id int,
+foreign key (tipoDocumentoCompra_id) references TIpoDocumentoCompra(idTipoDocumentoCompra),
 MontoTotal decimal (10, 2),
-FechaRegistro datetime default getdate())
+FechaRegistro datetime default getdate());
 go
 
 create table DETALLE_COMPRA(
-IdDetalleCompra int primary key identity,
-producto_id int,
+IdDetalleCompra int primary key identity(1,1),
+producto_id int not null,
 foreign key (producto_id) references PRODUCTO(IdProducto),
-compra_id int, 
+compra_id int not null, 
 foreign key (compra_id) references COMPRA(IdCompra),
-PrecioCompra decimal (10, 2) default 0,
-PrecioVenta decimal (10, 2) default 0,
-Cantidad int,
-MontoTotal decimal (10, 2),
+PrecioCompra decimal (10, 2) default 0.00,
+PrecioVenta decimal (10, 2) default 0.00,
+Cantidad int not null,
+MontoTotal decimal (10, 2) default 0.00,
 FechaRegistro datetime default getdate())
+go
+
+create table TipoDocumentoVenta(
+idTipoDocumentoVenta int identity(1,1) primary key,
+nombreTipoDocumentoVenta varchar (20) not null);
 go
 
 create table VENTA(
 IdVenta int primary key identity,
-usuario_id int,
+usuario_id int not null,
 foreign key (usuario_id) references USUARIO(IdUsuario),
-cliente_id int,
+cliente_id int not null,
 foreign key (cliente_id) references CLIENTE(IdCliente),
-MontoPago decimal (10, 2),
-MontoCambio decimal (10, 2),
-MontoTotal decimal (10, 2),
-FechaRegistro datetime default getdate())
+tipoDocumentoVenta_id int,
+foreign key (tipoDocumentoVenta_id) references TipoDocumentoVenta(idTipoDocumentoVenta),
+MontoPago decimal (10, 2) default 0.00,
+MontoCambio decimal (10, 2) default 0.00,
+MontoTotal decimal (10, 2) default 0.00,
+FechaRegistro datetime default getdate());
 go
+
 
 create table DETALLE_VENTA(
 IdDetalleVenta int primary key identity,
-venta_id int, 
+venta_id int not null, 
 foreign key (venta_id) references VENTA(IdVenta),
-producto_id int,
+producto_id int not null,
 foreign key (producto_id) references PRODUCTO(IdProducto),
 PrecioVenta decimal (10, 2) default 0,
-Cantidad int,
-SubTotal decimal (10, 2),
-FechaRegistro datetime default getdate())
+Cantidad int not null,
+SubTotal decimal (10, 2) default 0,
+FechaRegistro datetime default getdate());
 go
